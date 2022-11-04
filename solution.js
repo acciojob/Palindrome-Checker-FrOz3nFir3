4ac1f3eaff534e91491d57338b17f8b453e23a5a
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-11-04 11:12:52 +0530

    brute force approach solution
    trying the brute force approach solution first

--- solution.js
+++ solution.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+   // brute force approach
+  let stringArray = str.split("");
+  // reversing characters
+  stringArray.reverse();
+  
+  // palindrome is same as forward and backwards
+  return stringArray.join("") == str;
 }
 module.exports = palindrome
