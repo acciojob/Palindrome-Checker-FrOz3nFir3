7da2c3fa8fe40f77474be49c4ca60498581d2128
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-11-04 11:38:07 +0530

    optimized approach

--- solution.js
+++ solution.js
@@ -1,12 +1,23 @@
 // complete the given function
 
-function palindrome(str){
-   // brute force approach
-  let stringArray = str.split("");
-  // reversing characters
-  stringArray.reverse();
-  
-  // palindrome is same as forward and backwards
-  return stringArray.join("") == str;
+function palindrome(string){
+  // optimized approach O(N) Time and O(1) Space
+  let lowerCased = string.toLowerCase();
+
+  let startingPointer = 0;
+  let endingPointer = lowerCased.length -1;
+
+  // palindrome is same as forward and backwards thus checking first character and last character and repeating simultaneously for other part of the strings.
+  while(startingPointer < endingPointer){
+    let currentStartingCharacter = lowerCased[startingPointer];
+    let currentEndingCharacter = lowerCased[endingPointer];
+     
+    if(currentStartingCharacter != currentEndingCharacter) return false;
+     
+    startingPointer++;
+    endingPointer--;
+  }
+
+  return true;
 }
 module.exports = palindrome
